a3b84682300ab814ee47f2096529213330d23911
alvarozaragoza <alvaro@denzar.com.br>
2023-06-14 17:22:23 -0300

    Store with Pinia

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -2,7 +2,10 @@
 import { DateTime } from "luxon"
 import { ref, computed } from "vue"
 import { TimelinePost, today, thisWeek, thisMonth } from "../posts"
+import { usePosts } from "../stores/posts"
 import TimelineItem from "./TimelineItem.vue"
+
+const postsStore = usePosts()
 
 const periods = ["Today", "This Week", "This Month"] as const
 
@@ -38,6 +41,8 @@ function selectPeriod(period: Period) {
 </script>
 
 <template>
+    {{ postsStore.foo }}
+    <button @click="postsStore.updateFoo('bar')">Update</button>
     <nav class="is-primary panel">
         <span class="panel-tabs">
             <a 
