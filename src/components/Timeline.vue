349fb8928ff7bafceb5b1e67607e7104e1d819dd
alvarozaragoza <alvaro@denzar.com.br>
2023-06-15 15:10:15 -0300

    Store improved. App refactored. Node Server created

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,60 +1,24 @@
 <script setup lang="ts">
-import { DateTime } from "luxon"
-import { ref, computed } from "vue"
-import { TimelinePost, today, thisWeek, thisMonth } from "../posts"
 import { usePosts } from "../stores/posts"
 import TimelineItem from "./TimelineItem.vue"
+import { periods } from "../constants"
 
 const postsStore = usePosts()
-
-const periods = ["Today", "This Week", "This Month"] as const
-
-type Period = typeof periods[number]
-
-const selectedPeriod = ref<Period>("Today")
-
-const posts = computed<TimelinePost[]>( () => {
-    return [today, thisWeek, thisMonth]
-    .map(post => {
-        return {
-            ...post,
-            created: DateTime.fromISO(post.created)
-        }
-    })
-    .filter(post => {
-        if (selectedPeriod.value === "Today") {
-            return post.created >= DateTime.now().minus({day: 1 })
-        }
-        if (selectedPeriod.value === "This Week") {
-            return post.created >= DateTime.now().minus({week: 1 })
-        }
-        if (selectedPeriod.value === "This Month") {
-            return post.created >= DateTime.now().minus({month: 1 })
-        }
-        return post
-    })
-})
-
-function selectPeriod(period: Period) {
-    selectedPeriod.value = period
-}
 </script>
 
 <template>
-    {{ postsStore.foo }}
-    <button @click="postsStore.updateFoo('bar')">Update</button>
     <nav class="is-primary panel">
         <span class="panel-tabs">
             <a 
                 v-for="period of periods" 
                 :key="period" 
-                :class="{ 'is-active': period === selectedPeriod }"
-                @click="selectPeriod(period)">
+                :class="{ 'is-active': period === postsStore.selectedPeriod }"
+                @click="postsStore.setSelectedPeriod(period)">
                 {{ period }}
             </a>
         </span>    
         <TimelineItem
-            v-for="post of posts"
+            v-for="post of postsStore.filteredPosts"
             :key="post.id"
             :post="post"
         />
