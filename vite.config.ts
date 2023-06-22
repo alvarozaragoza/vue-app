115712756b63e35a022beeb14fb20c6f965f2d60
alvarozaragoza <alvaro@denzar.com.br>
2023-06-22 14:17:45 -0300

    FormInput testing program created

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'jsdom'
+  },
   server: {
     proxy: {
       '^/api/.*': {
